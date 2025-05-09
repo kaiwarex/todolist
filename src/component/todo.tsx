8cc5591019f6ff6703fd5aed7adf0193394d29f8
kaiwarex <pain.of.rebirth@gmail.com>
2025-05-09 23:52:01 +0900

    2回目コミット

---
+++ src/component/todo.tsx
@@ -0,0 +1,9 @@
+export default function Todo(){
+    return(
+        <>
+            <div>
+                <h1>TODOリスト作成予定地</h2>
+            </div>
+        </>
+    )
+}
--- src/component/todolist.tsx
+++
@@ -1,7 +0,0 @@
-export default function todo(){
-    <>
-        <div>
-            <h1>TODOリスト作成予定地</h2>
-        </div>
-    </>
-}
